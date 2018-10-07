c82ff821b05418d780e509ceafe1dec40ca92341
Slava <nosatskyy.slava@gmail.com>
2018-10-08 00:18:55 +0300

    adds homework_8, updates apple.com page

--- config/base.js
+++ config/base.js
@@ -13,6 +13,7 @@ const baseConf = (_path) => {
         lesson_5: ['./src/lesson_5/lesson_5.js'],
         lesson_6: ['./src/lesson_6/lesson_6.js'],
         homework_6: ['./src/homework_6/homework_6.js'],
+        homework_8: ['./src/homework_8/homework_8.js'],
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
