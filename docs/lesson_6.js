28736e571bf27538c3d937c3d7ceacb23172ea9d
Slava <nosatskyy.slava@gmail.com>
2018-10-22 20:15:50 +0300

    published apple.com

--- docs/lesson_6.js
+++ docs/lesson_6.js
@@ -81,6 +81,55 @@ module.exports = __webpack_require__(334);
 
 __webpack_require__(335);
 
+function switchVisibility(element) {
+    element.classList.toggle('footer__column-header_opened');
+    element.nextElementSibling.classList.toggle('footer__column-section_opened');
+}
+
+var switchVisibilityHeader1 = function switchVisibilityHeader1() {
+    switchVisibility(header_1);
+};
+
+var switchVisibilityHeader2 = function switchVisibilityHeader2() {
+    switchVisibility(header_2);
+};
+
+var switchVisibilityHeader3 = function switchVisibilityHeader3() {
+    switchVisibility(header_3);
+};
+
+var switchVisibilityHeader4 = function switchVisibilityHeader4() {
+    switchVisibility(header_4);
+};
+
+var switchVisibilityHeader5 = function switchVisibilityHeader5() {
+    switchVisibility(header_5);
+};
+
+var switchVisibilityHeader6 = function switchVisibilityHeader6() {
+    switchVisibility(header_6);
+};
+
+var switchVisibilityHeader7 = function switchVisibilityHeader7() {
+    switchVisibility(header_7);
+};
+
+var header_1 = document.querySelector('#header_1');
+var header_2 = document.querySelector('#header_2');
+var header_3 = document.querySelector('#header_3');
+var header_4 = document.querySelector('#header_4');
+var header_5 = document.querySelector('#header_5');
+var header_6 = document.querySelector('#header_6');
+var header_7 = document.querySelector('#header_7');
+
+header_1.onclick = switchVisibilityHeader1;
+header_2.onclick = switchVisibilityHeader2;
+header_3.onclick = switchVisibilityHeader3;
+header_4.onclick = switchVisibilityHeader4;
+header_5.onclick = switchVisibilityHeader5;
+header_6.onclick = switchVisibilityHeader6;
+header_7.onclick = switchVisibilityHeader7;
+
 /***/ }),
 
 /***/ 335:
--- src/lesson_6/lesson_6.js
+++ src/lesson_6/lesson_6.js
@@ -1,1 +1,56 @@
 import './lesson_6.scss';
+
+function switchVisibility(element) {
+    element.classList.toggle('footer__column-header_opened');
+    element.nextElementSibling.classList.toggle('footer__column-section_opened');
+}
+
+let switchVisibilityHeader1 = function () {
+    switchVisibility(header_1);
+};
+
+let switchVisibilityHeader2 = function () {
+    switchVisibility(header_2);
+};
+
+let switchVisibilityHeader3 = function () {
+    switchVisibility(header_3);
+};
+
+let switchVisibilityHeader4 = function () {
+    switchVisibility(header_4);
+};
+
+let switchVisibilityHeader5 = function () {
+    switchVisibility(header_5);
+};
+
+let switchVisibilityHeader6 = function () {
+    switchVisibility(header_6);
+};
+
+let switchVisibilityHeader7 = function () {
+    switchVisibility(header_7);
+};
+
+const header_1 = document.querySelector('#header_1');
+const header_2 = document.querySelector('#header_2');
+const header_3 = document.querySelector('#header_3');
+const header_4 = document.querySelector('#header_4');
+const header_5 = document.querySelector('#header_5');
+const header_6 = document.querySelector('#header_6');
+const header_7 = document.querySelector('#header_7');
+
+header_1.onclick = switchVisibilityHeader1;
+header_2.onclick = switchVisibilityHeader2;
+header_3.onclick = switchVisibilityHeader3;
+header_4.onclick = switchVisibilityHeader4;
+header_5.onclick = switchVisibilityHeader5;
+header_6.onclick = switchVisibilityHeader6;
+header_7.onclick = switchVisibilityHeader7;
+
+
+
+
+
+
